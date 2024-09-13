b6fde05fbe91c7fb3a97c107c2895d9c876e8c8a
hiiamcailyn <cailyntran@gmail.com>
2024-09-13 09:41:46 -0700

    hi

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -2,6 +2,16 @@ public class GradeCalculator
 {
   public static void main(String[] args)
   {
-    // insert code here
+    String course = "compsci";
+    int minWk = 30;
+    int hrWk = (30 * 60);
+    int test1 = 90;
+    int test2 = 60;
+    int test3 = 30;
+    int test4 = 100;
+    double quiz1 = 75.5;
+    double quiz2 = 80.5;
+    System.out.println(course + minWk + "minutes" + hrWk + "hours");
+
   }
 }
