8eb31bd961c884268e43e7d2c8506e087082a9ce
hiiamcailyn <cailyntran@gmail.com>
2024-09-13 09:58:28 -0700

    hi

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -9,23 +9,23 @@ public class GradeCalculator
     int hw2 = 60;
     int hw3 = 30;
     int hw4 = 100;
-    int avgHw = (90 + 60 + 30 + 100)/4
+    int avgHw = (90 + 60 + 30 + 100)/4;
     double quiz1 = 75.5;
     double quiz2 = 80.5;
     double avgQuiz = (75.5 + 80.5)/2;
-    double finalExam = 99.9
-    System.out.println(":course name :"  course );
-    System.out.println( " average time spent in a week for this course in minutes and hours: " +  minWk + " minutes or  " + hrWk + " hours");
-    System.out.println( "homework grades for this course: ");
+    double finalExam = 99.9;
+    System.out.println("Course name :" + course );
+    System.out.println( "Average time spent in a week for this course in minutes and hours: " +  minWk + " minutes or  " + hrWk + " hours");
+    System.out.println( "Homework grades for this course: ");
     System.out.println(hw1);
     System.out.println(hw2);
     System.out.println(hw3);
     System.out.println(hw4);
-    System.out.println( "quiz grades for this course: ");
+    System.out.println( "Quiz grades for this course: ");
     System.out.println(quiz1);
     System.out.println(quiz2);
-    System.out.println( " Average homework grade: " + avgHw);
-    System.out.println( " Average quiz grade: " + avgQuiz);
+    System.out.println( "Average homework grade: " + avgHw);
+    System.out.println( "Average quiz grade: " + avgQuiz);
     System.out.println( "Final exm grade for this course: " + finalExam);
 
 
