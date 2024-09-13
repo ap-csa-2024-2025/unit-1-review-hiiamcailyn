cb3cde13f5c5cc68e1e2eeef04396e61b0e8d50f
hiiamcailyn <cailyntran@gmail.com>
2024-09-13 09:56:52 -0700

    yes

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -5,13 +5,30 @@ public class GradeCalculator
     String course = "compsci";
     int minWk = 30;
     int hrWk = (30 * 60);
-    int test1 = 90;
-    int test2 = 60;
-    int test3 = 30;
-    int test4 = 100;
+    int hw1 = 90;
+    int hw2 = 60;
+    int hw3 = 30;
+    int hw4 = 100;
+    int avgHw = (90 + 60 + 30 + 100)/4
     double quiz1 = 75.5;
     double quiz2 = 80.5;
-    System.out.println(course + minWk + "minutes" + hrWk + "hours");
+    double avgQuiz = (75.5 + 80.5)/2;
+    double finalExam = 99.9
+    System.out.println(":course name :"  course );
+    System.out.println( " average time spent in a week for this course in minutes and hours: " +  minWk + " minutes or  " + hrWk + " hours");
+    System.out.println( "homework grades for this course: ");
+    System.out.println(hw1);
+    System.out.println(hw2);
+    System.out.println(hw3);
+    System.out.println(hw4);
+    System.out.println( "quiz grades for this course: ");
+    System.out.println(quiz1);
+    System.out.println(quiz2);
+    System.out.println( " Average homework grade: " + avgHw);
+    System.out.println( " Average quiz grade: " + avgQuiz);
+    System.out.println( "Final exm grade for this course: " + finalExam);
+
+
 
   }
 }
